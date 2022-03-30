d5cef564fa83bac25b18fea5f34030b8f36912fd
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2022-03-30 20:39:05 +0600

    Update development-2.js

--- development-2.js
+++ development-2.js
@@ -43,8 +43,8 @@ filters.forEach(button => {
 function showMenu(selectedTask){
     let menuDiv = selectedTask.parentElement.lastElementChild;
     menuDiv.classList.add("show");
-    document.addEventListener("click",e => {
-        if(e.target.tagName != "I" || e.target != selectedTask){
+    document.addEventListener("click",event => {
+        if(event.target.tagName != "I" || event.target != selectedTask){
             menuDiv.classList.remove("show");
         }
     });
