35d06f545227e493cad16878168ee13899bd9269
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2022-03-30 20:35:18 +0600

    Add files via upload

---
+++ development-2.js
@@ -0,0 +1,97 @@
+const taskInput = document.querySelector(".task-input input");
+const filters = document.querySelectorAll(".filters span");
+const clearButton = document.querySelector(".clear-button");
+const taskBox = document.querySelector(".task-box");
+let editId;
+let isEditTask = false;
+todos = JSON.parse(localStorage.getItem("todo-list"));
+function showTodo(filter){
+    let listTag = "";
+    if(todos){
+        todos.forEach((todo,id) => {
+            let completed = todo.status == "completed" ? "checked" : "";
+            if(filter == todo.status || filter == "all"){
+                listTag += `<li class="task">
+                            <label for="${id}">
+                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                                <p class="${completed}">${todo.name}</p>
+                            </label>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="task-menu">
+                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </li>`;
+            }
+        });
+    }
+    taskBox.innerHTML = listTag || `<span>You don't have any task here</span>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length ? clearButton.classList.remove("active") : clearButton.classList.add("active");
+    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
+}
+showTodo("all");
+filters.forEach(button => {
+    button.addEventListener("click",() => {
+        document.querySelector("span.active").classList.remove("active");
+        button.classList.add("active");
+        showTodo(button.id);
+    });
+});
+function showMenu(selectedTask){
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click",e => {
+        if(e.target.tagName != "I" || e.target != selectedTask){
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+function updateStatus(selectedTask){
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list",JSON.stringify(todos));
+}
+function editTask(taskId,textName){
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+function deleteTask(deleteId,filter){
+    isEditTask = false;
+    todos.splice(deleteId,1);
+    localStorage.setItem("todo-list",JSON.stringify(todos));
+    showTodo(filter);
+}
+clearButton.addEventListener("click",() => {
+    isEditTask = false;
+    todos.splice(0,todos.length);
+    localStorage.setItem("todo-list",JSON.stringify(todos));
+    showTodo();
+});
+taskInput.addEventListener("keyup",event => {
+    let userTask = taskInput.value.trim();
+    if(event.key == "Enter" && userTask){
+        if(!isEditTask) {
+            todos = !todos ? [] : todos;
+            let taskInfo = {name: userTask,status: "pending"};
+            todos.push(taskInfo);
+        } else {
+            isEditTask = false;
+            todos[editId].name = userTask;
+        }
+        taskInput.value = "";
+        localStorage.setItem("todo-list",JSON.stringify(todos));
+        showTodo(document.querySelector("span.active").id);
+    }
+});
